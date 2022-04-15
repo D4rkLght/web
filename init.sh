f1d1a9837dacf38697a6a818824c3f2b88d95af5
root <root@LAPTOP-4Q2I6VDI.localdomain>
2022-04-15 21:48:38 +0300

    add guni

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/hello.py   
+sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
