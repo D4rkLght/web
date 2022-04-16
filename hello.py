bff79c9a0e6d79ba062780a5c6a409c479ce7557
root <root@LAPTOP-4Q2I6VDI.localdomain>
2022-04-16 15:59:07 +0300

    conf

--- hello.py
+++ hello.py
@@ -3,6 +3,5 @@ def wsgi_application(environ, start_response):
     headers = [
         ('Content-Type', 'text/plain')
     ]
-    body = 'Hello, world!'
     start_response(status, headers)
-    return [body]
+    return environ["QUERY_STRING"]
