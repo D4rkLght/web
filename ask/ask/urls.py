010cc0f03f3f57e4647ffbe78b072b4c17aa0dc0
root <root@LAPTOP-4Q2I6VDI.localdomain>
2022-04-19 18:51:16 +0300

    url

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
-from django.conf.urls import include, url
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', include('qa.views')),
