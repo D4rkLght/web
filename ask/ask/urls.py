021c40fdd2d623ed6589e93c9dea9d5db67386be
root <root@LAPTOP-4Q2I6VDI.localdomain>
2022-04-19 18:41:47 +0300

    url

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -16,14 +16,13 @@ Including another URLconf
 from django.contrib import admin
 
 
-urlpatterns = (
-    'qa.views',
-    url(r'^$', 'test'),
-    url(r'^login/.*$', 'test', name='login'),
-    url(r'^signup/.*', 'test', name='signup'),
-    url(r'^question/(?P<id>[0-9]+)/$',
-        'test', name='question'),
-    url(r'^ask/.*', 'test', name='ask'),
-    url(r'^popular/.*', 'test', name='popular'),
-    url(r'^new/.*', 'test', name='new'),
-)
+urlpatterns = [
+    url(r'/', "qa.urls"),
+    url(r'^login\/.*$', "qa.urls"),
+    url(r'signup/', "qa.urls"),
+    url(r'questions/[\b+]/', "qa.urls"),
+    url(r'ask/', "qa.urls"),
+    url(r'popular/', "qa.urls"),
+    url(r'new/', "qa.urls"),
+    url(r'admin/', admin.site.urls),
+]
