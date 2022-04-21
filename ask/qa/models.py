e9616da54c2d1c6fa891653f1fb28c0de87410e7
root <root@LAPTOP-4Q2I6VDI.localdomain>
2022-04-21 20:32:20 +0300

    add models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,26 @@
+
 from django.db import models
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return Question
+
+    def popular(self):
+        return sorted(Question.rating)
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=225)
+    text = models.CharField(max_length=1000)
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.CharField(max_length=40)
+    likes = models.ManyToManyField()
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=500)
+    added_at = models.DateField()
+    question = models.OneToOneField()
+    author = models.CharField(max_length=40)
