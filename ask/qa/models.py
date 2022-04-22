d8d9ecc8ec1e4e43fb245a05e7642fe7e63bac20
root <root@LAPTOP-4Q2I6VDI.localdomain>
2022-04-22 14:28:51 +0300

    did migrate

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -12,10 +12,10 @@ class QuestionManager(models.Manager):
 
 class Question(models.Model):
     title = models.CharField(max_length=225)
-    text = models.TextField()
+    text = models.TextField(User)
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.CharField(max_length=40)
+    author = models.ForeignKey(max_length=40)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
 
