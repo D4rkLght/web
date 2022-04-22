8337b7d9f22cf78c36a155d4a447b5319b9489a1
root <root@LAPTOP-4Q2I6VDI.localdomain>
2022-04-22 14:33:05 +0300

    do

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -15,7 +15,7 @@ class Question(models.Model):
     text = models.TextField(User)
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(max_length=40)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
 
