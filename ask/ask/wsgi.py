47ac93dfae59ade7bfeaeb592a157d21a5c54a8f
root <root@LAPTOP-4Q2I6VDI.localdomain>
2022-05-05 16:03:03 +0300

    wsgi changed

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ask.settings')
+sys.path.append('/home/box/web/ask')
+sys.path.append('/home/box/web/ask/ask')
 
 application = get_wsgi_application()
