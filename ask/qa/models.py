5e17a982bfade3028a8ee97902b68dc9dc7d5834
root <root@LAPTOP-4Q2I6VDI.localdomain>
2022-05-05 15:25:35 +0300

    models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -14,15 +13,21 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=225)
-    text = models.TextField(User)
+    text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
+    def __str__(self):
+        return self.title
+
 
 class Answer(models.Model):
-    text = models.TextField(User)
+    text = models.TextField()
     added_at = models.DateField()
     question = models.OneToOneField("Question", on_delete=models.PROTECT)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.text
