5f3027e6e72a380331e377b92a4f1942aae5c114
root <root@LAPTOP-4Q2I6VDI.localdomain>
2022-05-05 15:35:53 +0300

    views

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,38 @@
+from django.http import Http404
+from django.core.paginator import Paginator
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+from . import models
+
+
+def home(request, *args, **kwargs):
+    questions_list = models.QuestionManager.new(models.Question.objects.all())
+    paginator = Paginator(questions_list, 10)
+
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'index.html', {'posts': posts})
+
+
+def ask(request, *args, **kwargs):
+
+    return render(request, 'ask.html')
+
+
+def popular(request, *args, **kwargs):
+    questions_list = models.QuestionManager.popular(
+        models.Question.objects.all())
+    paginator = Paginator(questions_list, 10)
+
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'popular.html', {'posts': posts})
+
+
+def question(request, *args, **kwargs):
+    posts = models.Question.objects.all()
+    post_id = request.path.split('/')[-2]
+    for post in posts:
+        if post.id == int(post_id):
+            return render(request, 'question.html', {'post': post})
